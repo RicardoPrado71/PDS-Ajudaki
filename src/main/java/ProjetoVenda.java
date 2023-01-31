b7da0d99dc825ce797706f2a6c10c28aff23b04b
Ricardo Prado <ricardofprado98@gmail.com>
2023-01-31 20:27:15 -0300

    Primeiro comentario

---
+++ src/main/java/ProjetoVenda.java
@@ -0,0 +1,8 @@
+import entity.Produto;
+
+public class ProjetoVenda {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        Produto produto = new Produto();
+        produto.
+    }
---
+++ src/main/java/entity/Produto.java
@@ -0,0 +1,61 @@
+package entity;
+
+public class Produto {
+
+    private Double quantEstoque;
+    private Double vlrVendas;
+    private Double vlrCusto;
+    private String nomeProduto;
+    private String unidade;
+    private Double peso;
+
+    public Double getQuantEstoque() {
+        return quantEstoque;
+    }
+
+    public void setQuantEstoque(Double quantEstoque) {
+        this.quantEstoque = quantEstoque;
+    }
+
+    public Double getVlrVendas() {
+        return vlrVendas;
+    }
+
+    public void setVlrVendas(Double vlrVendas) {
+        this.vlrVendas = vlrVendas;
+    }
+
+    public Double getVlrCusto() {
+        return vlrCusto;
+    }
+
+    public void setVlrCusto(Double vlrCusto) {
+        this.vlrCusto = vlrCusto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+
+}
