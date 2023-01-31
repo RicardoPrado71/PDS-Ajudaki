af45550c802ab0f5bd38e6e9c1709ebc8db5e949
Ricardo Prado <ricardofprado98@gmail.com>
2023-01-31 20:54:58 -0300

    Primeira Class

--- src/main/java/ProjetoVenda.java
+++ src/main/java/ProjetoVenda.java
@@ -4,5 +4,10 @@ public class ProjetoVenda {
     public static void main(String[] args) {
         System.out.println("hello world!");
         Produto produto = new Produto();
-        produto.
+        produto.setNomeProduto("Coca cola");
+        produto.setPeso(500.0);
+        produto.setUnidade("Fardo");
+        produto.setVlrVendas(20.0);
+        produto.setVlrCusto(5.0);
+        produto.setQuantEstoque(20.0);
     }
